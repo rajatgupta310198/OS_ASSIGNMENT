342625b37fde8ef1b7063cc3836e30e15e1e3bc5
Rajat Gupta <rajat310198@outlook.com>
2017-03-03 02:55:20 +0530

    Update sjf.c

--- scheduling algorithms/SJF/sjf.c
+++ scheduling algorithms/SJF/sjf.c
@@ -12,11 +12,8 @@ int main()
 	for(i=0;i<n;i++)
 		{
 			pcb.pid = i+1;
-			pcb.in_time = i;
-			printf("\nEnter burst time :");
-			float t;
-			scanf("%f",&t);
-			pcb.burst = t;
+			pcb.in_time = rand()%100;
+			pcb.burst = 10 - (float)rand()/RAND_MAX;
 			enq(&readyq,pcb);
 		}
  	printf("\n");
